5b7050cb9b2ff57da8c42b84450064d877aac770
Tyler Hunt <tyhunt04@gmail.com>
2019-04-26 04:04:59 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -29,6 +29,6 @@ class PostsController < ApplicationController
 
 	private
 
-	def post_params
-		params.require(:post).permit(:title, :description)
+	def post_params(*args)
+		params.require(:post).permit(*args)
 end
